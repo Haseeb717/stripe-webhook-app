caae28aa5ca8fe8df49eef18868b41946ffeada0
Haseeb717 <haseeb.ahmad717@gmail.com>
2024-08-05 13:06:41 +0200

    Specs changes

--- spec/interactors/handle_subscription_created_spec.rb
+++ spec/interactors/handle_subscription_created_spec.rb
@@ -3,18 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe HandleSubscriptionCreated, type: :interactor do
-  let(:subscription) { create(:subscription) }
   let(:event) do
-    StripeMock.mock_webhook_event('customer.subscription.created',
-                                  data: { object: { id: subscription.stripe_subscription_id,
-                                                    customer: subscription.stripe_customer_id } })
-  end
-  let(:subscription_record) do
-    Subscription.find_or_initialize_by(stripe_subscription_id: subscription.stripe_subscription_id)
+    StripeMock.mock_webhook_event('customer.subscription.created')
   end
 
   before do
-    allow(Subscription).to receive(:find_or_initialize_by).and_return(subscription_record)
     StripeMock.start
   end
 
@@ -25,26 +18,12 @@ RSpec.describe HandleSubscriptionCreated, type: :interactor do
   describe '.call' do
     context 'when event is valid and subscription is successfully created or updated' do
       it 'creates or updates the subscription record' do
-        expect(subscription_record).to receive(:update!).with(
-          stripe_customer_id: event.data.object.customer,
-          status: 'unpaid'
-        )
+        expect do
+          HandleSubscriptionCreated.call(event:)
+        end.to change { Subscription.count }.by(1)
 
         result = HandleSubscriptionCreated.call(event:)
         expect(result).to be_success
-        expect(result.subscription_record).to eq(subscription_record)
-      end
-    end
-
-    context 'when an error occurs while processing' do
-      before do
-        allow(subscription_record).to receive(:update!).and_raise(StandardError, 'Update failed')
-      end
-
-      it 'fails with an error message' do
-        result = HandleSubscriptionCreated.call(event:)
-        expect(result).to be_failure
-        expect(result.error).to eq('Update failed')
       end
     end
   end
