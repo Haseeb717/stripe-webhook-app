0f4d179b6c7df0667b570160c4442bbad86761b7
Haseeb717 <haseeb.ahmad717@gmail.com>
2024-08-05 07:39:46 +0200

    Added aasm for state management

--- app/interactors/handle_subscription_deleted.rb
+++ app/interactors/handle_subscription_deleted.rb
@@ -6,12 +6,27 @@ class HandleSubscriptionDeleted
 
   def call
     subscription = context.event.data.object
-    subscription_record = Subscription.find_by(stripe_id: subscription.id)
-    return context.fail!(error: 'Subscription not found or not paid') unless subscription_record&.paid?
+    subscription_record = find_subscription(subscription.id)
+    return unless subscription_record
 
-    subscription_record.update!(status: 'canceled')
-    context.subscription_record = subscription_record
+    cancel_subscription(subscription_record)
   rescue StandardError => e
     context.fail!(error: e.message)
+  end
+
+  private
+
+  def find_subscription(subscription_id)
+    subscription_record = Subscription.find_by(stripe_subscription_id: subscription_id)
+    context.fail!(error: 'Subscription not found or not paid') unless subscription_record&.paid?
+    subscription_record
+  end
+
+  def cancel_subscription(subscription_record)
+    if subscription_record.may_cancel?
+      subscription_record.cancel!
+    else
+      context.fail!(error: 'Subscription cannot be canceled')
+    end
   end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -14,6 +14,24 @@
 
 # Model representing a subscription.
 class Subscription < ApplicationRecord
+  include AASM
+
   validates :stripe_subscription_id, presence: true, uniqueness: true
   validates :status, presence: true
+
+  enum status: { unpaid: 'unpaid', paid: 'paid', canceled: 'canceled' }
+
+  aasm column: 'status' do
+    state :unpaid, initial: true
+    state :paid
+    state :canceled
+
+    event :pay do
+      transitions from: :unpaid, to: :paid
+    end
+
+    event :cancel do
+      transitions from: :paid, to: :canceled
+    end
+  end
 end
